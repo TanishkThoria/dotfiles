2c283890b35e739dbc9d42bd2c9dec399ca53743
Tanishk <tanishkthoria@gmail.com>
2023-01-31 17:31:55 -0800

    Updated config menu

--- bash-scripts/rofi-config-menu.sh
+++ bash-scripts/rofi-config-menu.sh
@@ -1,53 +1,42 @@
 #!/usr/bin/bash
 
-terminal = "kitty"
+TERMINAL="kitty"
 
 # Give user a rofi prompt
-choice=$(echo "NeoVIM|Awesome WM|Bashrc|Rofi|Config Menu" | rofi -sep '|' -dmenu -p "Config:")
+choice=$(echo "NeoVIM|i3 WM|Bashrc|Rofi|Config Menu" | rofi -sep '|' -dmenu -p "Config: " )
 
 # Set lua_path accordingly (NeoVIM config modules)
 export LUA_PATH="$HOME/.config/nvim/?.lua;$LUA_PATH"
 
-if [ "$choice" = "Awesome WM" ]; then
-    # Choose one of the configs
-    choice=$(echo "rc.lua|theme.lua" | rofi -sep '|' -dmenu -p "Awesome WM:")
-
-    if [ "$choice" = "rc.lua" ]; then
-        terminal .. -- nvim ~/.config/awesome/rc.lua
-
-    elif [ "$choice" = "theme.lua" ]; then
-        terminal .. -- nvim ~/.config/awesome/themes/powerarrow/theme.lua
-    fi
+if [ "$choice" = "i3 WM" ]; then
+    $TERMINAL -- nvim ~/.config/i3/config 
 
 elif [ "$choice" = "NeoVIM" ]; then
     # Choose one of the configs
-    choice=$(echo "init.lua|settings.lua|highlights.lua|plugins.lua|oldinit.vim" | rofi -sep '|' -dmenu -p "NeoVIM:")
+    choice=$(echo "init.lua|settings.lua|highlights.lua|plugins.lua" | rofi -sep '|' -dmenu -p "NeoVIM: " )
     
     if [ "$choice" = "init.lua" ]; then
-        terminal .. -- nvim ~/.config/nvim/init.lua
+        $TERMINAL -- nvim ~/.config/nvim/init.lua 
 
     elif [ "$choice" = "settings.lua" ]; then
-        terminal .. -- nvim ~/.config/nvim/settings.lua
+        $TERMINAL -- nvim ~/.config/nvim/settings.lua 
 
     elif [ "$choice" = "highlights.lua" ]; then
-        terminal .. -- nvim ~/.config/nvim/highlights.lua
+        $TERMINAL -- nvim ~/.config/nvim/highlights.lua 
 
     elif [ "$choice" = "plugins.lua" ]; then
-        terminal .. -- nvim ~/.config/nvim/highlights.lua
-
-    elif [ "$choice" = "oldinit.vim" ]; then
-        terminal .. -- nvim ~/.config/nvim/oldinit.vim
+        $TERMINAL -- nvim ~/.config/nvim/highlights.lua 
     
     fi
 
 elif [ "$choice" = "Bashrc" ]; then
-    terminal .. -- nvim ~/.bashrc
+    $TERMINAL -- nvim ~/.bashrc 
     
 elif [ "$choice" = "Rofi" ]; then
-	terminal .. -- nvim ~/.config/rofi/config.rasi
+    $TERMINAL -- nvim ~/.config/rofi/config.rasi 
 
 elif [ "$choice" = "Config Menu" ]; then
-    terminal .. -- nvim ~/bash-scripts/rofi-config-menu.sh
+    $TERMINAL -- nvim ~/bash-scripts/rofi-config-menu.sh 
     
 fi
 
