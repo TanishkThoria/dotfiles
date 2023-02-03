3973800716c149ba3e2f85014843256819948999
Tanishk <tanishkthoria@gmail.com>
2023-02-02 19:04:05 -0800

    Adapted for easier modifications

--- bash-scripts/rofi-config-menu.sh
+++ bash-scripts/rofi-config-menu.sh
@@ -1,34 +1,37 @@
 #!/usr/bin/bash
 
+# Applications
 TERMINAL="kitty"
+sep="--" # see man page of your terminal emulator for how to run programs
+EDITOR="nvim"
 
 # Give user a rofi prompt
 choice=$(echo "i3 WM|Polybar|NeoVIM|Kitty|Bashrc|Starship|Rofi|Config Menu" | rofi -sep '|' -dmenu -p "Config: " )
 
 # Set lua_path accordingly (NeoVIM config modules)
-export LUA_PATH="$HOME/.config/nvim/?.lua;$LUA_PATH"
+export LUA_PATH="$HOME/.config/$EDITOR/?.lua;$LUA_PATH"
 
 if [ "$choice" = "i3 WM" ]; then
-    $TERMINAL -- nvim ~/.config/i3/config 
+    $TERMINAL $sep $EDITOR ~/.config/i3/config 
 
 elif [ "$choice" = "Polybar" ]; then
-    $TERMINAL -- nvim ~/.config/polybar/config.ini
+    $TERMINAL $sep $EDITOR ~/.config/polybar/config.ini
 
 elif [ "$choice" = "NeoVIM" ]; then
     # Choose one of the configs
     choice=$(echo "init.lua|settings.lua|highlights.lua|plugins.lua" | rofi -sep '|' -dmenu -p "NeoVIM: " )
     
     if [ "$choice" = "init.lua" ]; then
-        $TERMINAL -- nvim ~/.config/nvim/init.lua 
+        $TERMINAL $sep $EDITOR ~/.config/nvim/init.lua 
 
     elif [ "$choice" = "settings.lua" ]; then
-        $TERMINAL -- nvim ~/.config/nvim/settings.lua 
+        $TERMINAL $sep $EDITOR ~/.config/nvim/settings.lua 
 
     elif [ "$choice" = "highlights.lua" ]; then
-        $TERMINAL -- nvim ~/.config/nvim/highlights.lua 
+        $TERMINAL $sep $EDITOR ~/.config/nvim/highlights.lua 
 
     elif [ "$choice" = "plugins.lua" ]; then
-        $TERMINAL -- nvim ~/.config/nvim/plugins.lua 
+        $TERMINAL $sep $EDITOR ~/.config/nvim/plugins.lua 
     
     fi
 
@@ -37,27 +40,27 @@ elif [ "$choice" = "Kitty" ]; then
     choice=$(echo "kitty.conf|dracula-dark-colors.conf|elenapan-colors.conf" | rofi -sep '|' -dmenu -p "Kitty: " )
     
     if [ "$choice" = "kitty.conf" ]; then
-        $TERMINAL -- nvim ~/.config/kitty/kitty.conf
+        $TERMINAL $sep $EDITOR ~/.config/kitty/kitty.conf
 
     elif [ "$choice" = "dracula-dark-colors.conf" ]; then
-        $TERMINAL -- nvim ~/.config/kitty/dracula-dark-colors.conf
+        $TERMINAL $sep $EDITOR ~/.config/kitty/dracula-dark-colors.conf
     
     elif [ "$choice" = "elenapan-colors.conf" ]; then
-        $TERMINAL -- nvim ~/.config/kitty/elenapan-colors.conf
+        $TERMINAL $sep $EDITOR ~/.config/kitty/elenapan-colors.conf
 
     fi
 
 elif [ "$choice" = "Bashrc" ]; then
-    $TERMINAL -- nvim ~/.bashrc 
+    $TERMINAL $sep $EDITOR ~/.bashrc 
     
 elif [ "$choice" = "Starship" ]; then
-    $TERMINAL -- nvim ~/.config/starship/starship.toml
+    $TERMINAL $sep $EDITOR ~/.config/starship/starship.toml
 
 elif [ "$choice" = "Rofi" ]; then
-    $TERMINAL -- nvim ~/.config/rofi/config.rasi 
+    $TERMINAL $sep $EDITOR ~/.config/rofi/config.rasi 
 
 elif [ "$choice" = "Config Menu" ]; then
-    $TERMINAL -- nvim ~/bash-scripts/rofi-config-menu.sh 
+    $TERMINAL $sep $EDITOR ~/bash-scripts/rofi-config-menu.sh 
     
 fi
 
