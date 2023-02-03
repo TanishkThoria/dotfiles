2a920515f3c1ebcfa4bfd91a21af36c1ce4e4916
Tanishk <tanishkthoria@gmail.com>
2023-02-02 17:11:21 -0800

    Changed bar name and added multimonitor support

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,11 @@
 # Otherwise you can use the nuclear option:
 killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do : ; done
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-i3 &
+done
 
 echo "Bars launched..."
