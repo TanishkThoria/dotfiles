6c56c1fcb9b8d95a67509de19b44a6d0535b179a
Tanishk <tanishkthoria@gmail.com>
2023-02-05 11:52:48 -0800

    Added emacs

--- bash-scripts/rofi-config-menu.sh
+++ bash-scripts/rofi-config-menu.sh
@@ -3,10 +3,10 @@
 # Applications
 TERMINAL="kitty"
 sep="--" # see man page of your terminal emulator for how to run programs
-EDITOR="nvim"
+EDITOR="emacs"
 
 # Give user a rofi prompt
-choice=$(echo "i3 WM|Polybar|NeoVIM|Kitty|Bashrc|Starship|Rofi|Config Menu" | rofi -sep '|' -dmenu -p "Config: " )
+choice=$(echo "i3 WM|Polybar|Doom Emacs|NeoVIM|Kitty|Bashrc|Starship|Rofi|Config Menu" | rofi -sep '|' -dmenu -p "Config: " )
 
 # Set lua_path accordingly (NeoVIM config modules)
 export LUA_PATH="$HOME/.config/$EDITOR/?.lua;$LUA_PATH"
@@ -16,6 +16,21 @@ if [ "$choice" = "i3 WM" ]; then
 
 elif [ "$choice" = "Polybar" ]; then
     $TERMINAL $sep $EDITOR ~/.config/polybar/config.ini
+
+elif [ "$choice" = "Doom Emacs" ]; then
+    # Choose one of the configs
+    choice=$(echo "init.el|config.el|packages.el" | rofi -sep '|' -dmenu -p "Doom Emacs: " )
+
+    if [ "$choice" = "init.el" ]; then
+        $TERMINAL $sep $EDITOR ~/.doom.d/init.el
+
+    elif [ "$choice" = "config.el" ]; then
+        $TERMINAL $sep $EDITOR ~/.doom.d/config.el
+
+    elif [ "$choice" = "packages.el" ]; then
+        $TERMINAL $sep $EDITOR ~/.doom.d/packages.el
+
+    fi
 
 elif [ "$choice" = "NeoVIM" ]; then
     # Choose one of the configs
