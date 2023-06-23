e061ae609ab2070e63f3dc215077db78b922d334
Tanishk <tanishkthoria@gmail.com>
2023-06-23 10:43:30 -0700

    Added C++

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -41,6 +41,15 @@
 
 (add-hook 'rust-mode-hook 'lsp-deferred)
 
+;; LSP-mode with clangd for c++
+(setq lsp-clients-clangd-args '("-j=3"
+				"--background-index"
+				"--clang-tidy"
+				"--completion-style=detailed"
+				"--header-insertion=never"
+				"--header-insertion-decorators=0"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
